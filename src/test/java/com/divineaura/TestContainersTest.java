54100a6f45dda80b981461431a7a7475de16a074
Vignesh Vellingiri <vvellingiri@atlassian.com>
2023-06-01 22:10:28 +0530

    Junit TestContainer setup

---
+++ src/test/java/com/divineaura/TestContainersTest.java
@@ -0,0 +1,17 @@
+package com.divineaura;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public class TestContainersTest extends AbstractTestContainer {
+
+    @Test
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
+
+}
