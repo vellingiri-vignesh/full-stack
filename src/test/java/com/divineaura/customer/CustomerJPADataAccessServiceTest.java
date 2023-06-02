050d4aeb8ceb8ad6e07fcee35d11ba213b7d73ba
Vignesh Vellingiri <vvellingiri@atlassian.com>
2023-06-02 11:31:28 +0530

    DAO Mock testing for JPADataAccessService

---
+++ src/test/java/com/divineaura/customer/CustomerJPADataAccessServiceTest.java
@@ -0,0 +1,112 @@
+package com.divineaura.customer;
+
+import static org.mockito.Mockito.verify;
+
+import com.github.javafaker.Faker;
+import java.util.UUID;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class CustomerJPADataAccessServiceTest {
+
+    private CustomerJPADataAccessService underTest;
+    private AutoCloseable autoCloseable;
+    @Mock
+    private CustomerRepository customerRepository;
+    private Faker FAKER = new Faker();
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        underTest = new CustomerJPADataAccessService(customerRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+
+    @Test
+    void selectAllCustomers() {
+        //When
+        underTest.selectAllCustomers();
+
+        //Then
+        verify(customerRepository).findAll();
+    }
+
+    @Test
+    void selectCustomerById() {
+        //Given
+        int id = 3;
+
+        //When
+        underTest.selectCustomerById(id);
+
+        //Then
+        verify(customerRepository).findById(id);
+    }
+
+    @Test
+    void insertCustomer() {
+        //Given
+        Customer customer = new Customer(FAKER.name().fullName(), FAKER.internet().safeEmailAddress(), 22);
+
+        //When
+        underTest.insertCustomer(customer);
+
+        //Then
+        verify(customerRepository).save(customer);
+    }
+
+    @Test
+    void existsCustomerWithEmail() {
+        //Given
+        String email = UUID.randomUUID().toString();
+
+        //When
+        underTest.existsCustomerWithEmail(email);
+
+        //Then
+        verify(customerRepository).existsCustomerByEmail(email);
+    }
+
+    @Test
+    void existsCustomerWithId() {
+        //Given
+        int id = 2;
+
+        //When
+        underTest.existsCustomerWithId(id);
+
+        //Then
+        verify(customerRepository).existsCustomerById(id);
+    }
+
+    @Test
+    void deleteCustomerById() {
+        //Given
+        int id = 2;
+
+        //When
+        underTest.deleteCustomerById(id);
+
+        //Then
+        verify(customerRepository).deleteById(id);
+    }
+
+    @Test
+    void updateCustomer() {
+        //Given
+        Customer customer = new Customer(FAKER.name().fullName(), FAKER.internet().safeEmailAddress(), 22);
+
+        //When
+        underTest.updateCustomer(customer);
+
+        //Then
+        verify(customerRepository).save(customer);
+    }
+}
